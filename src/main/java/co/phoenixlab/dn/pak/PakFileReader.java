cc93128958d037afdb63f5422b0eab90c2480b14
Vincent Zhang <vzhang@purdue.edu>
2015-03-16 19:25:10 -0400

    Strip the old debug messages

--- src/main/java/co/phoenixlab/dn/pak/PakFileReader.java
+++ src/main/java/co/phoenixlab/dn/pak/PakFileReader.java
@@ -69,7 +69,6 @@ public class PakFileReader implements AutoCloseable {
         String[] strs = path.split("\\\\", 2);
         if (strs.length == 1) {
             parent.getChildren().put(strs[0], new FileEntry(fileInfo.getFileName(), parent, fileInfo));
-//            System.err.println("Insertion at " + fileInfo.getFileName() + " w/ parent " + parent.name);
             return;
         }
         Entry newEntry = parent.getChildren().get(strs[0]);
@@ -80,7 +79,6 @@ public class PakFileReader implements AutoCloseable {
             throw new IllegalArgumentException("Cannot replace an existing file with a directory");
         } else {
             dirEntry = new DirEntry(strs[0], parent);
-//            System.err.println("Insert direntry " + strs[0] + " w/ parent " + parent.name);
             parent.getChildren().put(strs[0], dirEntry);
         }
         insert(strs[1], dirEntry, fileInfo);
