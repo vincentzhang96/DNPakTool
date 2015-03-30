5c167c715c2257515a08e6480d0d9979f6cad61c
Vincent Zhang <vzhang@purdue.edu>
2015-03-30 12:42:32 -0400

    Implemented DataRow

--- src/main/java/co/phoenixlab/dn/dnt/DataRow.java
+++ src/main/java/co/phoenixlab/dn/dnt/DataRow.java
@@ -24,32 +24,135 @@
 
 package co.phoenixlab.dn.dnt;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 public class DataRow {
 
-    private Map<String, Object> entries;
-    private Object[] entryArray;
+    class Pair {
+        final Object object;
+        final Class<?> type;
+
+        public Pair(Object object, Class<?> type) {
+            this.object = object;
+            this.type = type;
+        }
+    }
+
+    private Map<String, Pair> entries;
+    private Pair[] entryArray;
 
     protected DataRow(String[] columnNames) {
         entries = new HashMap<>(columnNames.length);
-        entryArray = new Object[columnNames.length];
+        entryArray = new Pair[columnNames.length];
         for (String s : columnNames) {
             entries.put(s, null);
         }
     }
 
+    public Map<String, Pair> getEntries() {
+        return Collections.unmodifiableMap(entries);
+    }
+
+    public Pair[] getEntryArray() {
+        return entryArray;
+    }
+
+    public long getUint32(String columnName) {
+        return getAs(columnName, Long.class);
+    }
+
+    public String getString(String columnName) {
+        return getAs(columnName, String.class);
+    }
+
+    public boolean getBool(String columnName) {
+        return getAs(columnName, Boolean.class);
+    }
+
+    public int getInt(String columnName) {
+        return getAs(columnName, Integer.class);
+    }
+
+    public float getFloat(String columnName) {
+        return getAs(columnName, Float.class);
+    }
+    
+    public double getDouble(String columnName) {
+        return getAs(columnName, Double.class);
+    }
+
+    public long getUint32(int columnNum) {
+        return getAs(columnNum, Long.class);
+    }
+
+    public String getString(int columnNum) {
+        return getAs(columnNum, String.class);
+    }
+
+    public boolean getBool(int columnNum) {
+        return getAs(columnNum, Boolean.class);
+    }
+
+    public int getInt(int columnNum) {
+        return getAs(columnNum, Integer.class);
+    }
+
+    public float getFloat(int columnNum) {
+        return getAs(columnNum, Float.class);
+    }
+
+    public double getDouble(int columnNum) {
+        return getAs(columnNum, Double.class);
+    }
+
+    public Class<?> getType(String columnName) {
+        Pair pair = entries.get(columnName);
+        if (pair == null) {
+            throw new NoSuchElementException(columnName);
+        }
+        return pair.object.getClass();
+    }
+
+    public Class<?> getType(int columnNum) {
+        Pair pair = entryArray[columnNum];
+        if (pair == null) {
+            throw new IllegalStateException("Column " + columnNum + " not initialized yet!");
+        }
+        return pair.object.getClass();
+    }
+
     private <T> T getAs(String columnName, Class<T> tClass) {
-        Object o = entries.get(columnName);
+        Pair pair = entries.get(columnName);
+        if (pair == null) {
+            throw new NoSuchElementException(columnName);
+        }
         try {
-            T t = tClass.cast(o);
-            return t;
+            return tClass.cast(pair.object);
         } catch (ClassCastException e) {
             throw new IllegalArgumentException("Column " + columnName + " is not type " + tClass.getSimpleName());
         }
     }
 
+    private <T> T getAs(int columnNum, Class<T> tClass) {
+        Pair pair = entryArray[columnNum];
+        if (pair == null) {
+            throw new IllegalStateException("Column " + columnNum + " not initialized yet!");
+        }
+        try {
+            return tClass.cast(pair.object);
+        } catch (ClassCastException e) {
+            throw new IllegalArgumentException("Column " + columnNum + " is not type " + tClass.getSimpleName());
+        }
+    }
+
+    protected void put(String columnName, int columnNum, Object o) {
+        Pair pair = new Pair(o, o.getClass());
+        entries.put(columnName, pair);
+        entryArray[columnNum] = pair;
+    }
 
 
 
